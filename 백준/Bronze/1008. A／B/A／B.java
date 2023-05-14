f3f065ab41e35eb70a6bff7b89cf5c10e197bf7d
woojinho <129517591+jinho-22@users.noreply.github.com>
2023-05-14 14:04:53 +0900

    [Bronze V] Title: A/B, Time: 220 ms, Memory: 17988 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double x = sc.nextDouble();
+        double y = sc.nextDouble();
+        System.out.println(x/y);
+    }
+}
